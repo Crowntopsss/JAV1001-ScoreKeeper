2402f73b838620a890b2430f301a89c91445f33f
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-07-06 19:49:36 +0100

    Added actions to the buttons

--- app/src/main/java/com/example/temitope_adebiyi_score/MainActivity.kt
+++ app/src/main/java/com/example/temitope_adebiyi_score/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.temitope_adebiyi_score
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.temitope_adebiyi_score.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var team1Score = 0
+    private var team2Score = 0
+    private var scoreIncrement = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.team1IncreaseButton.setOnClickListener {
+            team1Score += scoreIncrement
+            binding.team1ScoreTextView.text = team1Score.toString()
+        }
+
+        binding.team1DecreaseButton.setOnClickListener {
+            team1Score -= scoreIncrement
+            binding.team1ScoreTextView.text = team1Score.toString()
+        }
+
+        binding.team2IncreaseButton.setOnClickListener {
+            team2Score += scoreIncrement
+            binding.team2ScoreTextView.text = team2Score.toString()
+        }
+
+        binding.team2DecreaseButton.setOnClickListener {
+            team2Score -= scoreIncrement
+            binding.team2ScoreTextView.text = team2Score.toString()
+        }
+
+        binding.score1RadioButton.setOnClickListener {
+            scoreIncrement = 1
+        }
+
+        binding.score2RadioButton.setOnClickListener {
+            scoreIncrement = 2
+        }
+
+        binding.score3RadioButton.setOnClickListener {
+            scoreIncrement = 3
+        }
     }
 }
